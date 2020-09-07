b88ef8922c91be600abd903e63f493b330473422
koesnij <m_jinseok@naver.com>
2020-09-07 21:12:43 +0900

    Message Navigation II : Header Button Component

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
@@ -7,6 +7,7 @@ import Home from '../screens/Tabs/Home';
 import Search from '../screens/Tabs/Search';
 import Notifications from '../screens/Tabs/Notifications';
 import Profile from '../screens/Tabs/Profile';
+import MessagesLink from '../components/MessagesLink';
 
 const stackFactory = (initialRoute, customConfig) =>
   createStackNavigator({
@@ -20,11 +21,7 @@ export default createBottomTabNavigator({
   Home: {
     screen: stackFactory(Home, {
       title: 'Home',
-      headerRight: () => (
-        <TouchableOpacity>
-          <Text>Hello</Text>
-        </TouchableOpacity>
-      ),
+      headerRight: () => <MessagesLink />,
     }),
   },
   Search: {
