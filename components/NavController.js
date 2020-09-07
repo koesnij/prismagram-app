08064179c6abc8f665ef76c451009e3b3269de5a
koesnij <m_jinseok@naver.com>
2020-09-07 20:33:41 +0900

    #11.3 Photo Navigation

--- components/NavController.js
+++ components/NavController.js
@@ -3,13 +3,13 @@ import { View } from 'react-native';
 
 import { useIsLoggedIn } from '../AuthContext';
 import AuthNavigation from '../navigation/AuthNavigation';
-import TabNavigation from '../navigation/TabNavigation';
+import MainNavigation from '../navigation/MainNavigation';
 
 export default () => {
   const isLoggedIn = true;
   return (
     <View style={{ flex: 1 }}>
-      {isLoggedIn ? <TabNavigation /> : <AuthNavigation />}
+      {isLoggedIn ? <MainNavigation /> : <AuthNavigation />}
     </View>
   );
 };
