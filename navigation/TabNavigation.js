dc0bfa5386a4fccb7b447d66696fb8b8126ba5fd
koesnij <m_jinseok@naver.com>
2020-09-07 20:57:37 +0900

    #11.4 Messages Navigation I : Stack Navigation in Tab Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,14 +1,37 @@
-import { View } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import Home from '../screens/Home';
-import Search from '../screens/Search';
-import Notifications from '../screens/Notifications';
-import Profile from '../screens/Profile';
+import Home from '../screens/Tabs/Home';
+import Search from '../screens/Tabs/Search';
+import Notifications from '../screens/Tabs/Notifications';
+import Profile from '../screens/Tabs/Profile';
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    initialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig },
+    },
+  });
 
 export default createBottomTabNavigator({
-  Home,
-  Search,
+  Home: {
+    screen: stackFactory(Home, {
+      title: 'Home',
+      headerRight: () => (
+        <TouchableOpacity>
+          <Text>Hello</Text>
+        </TouchableOpacity>
+      ),
+    }),
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: 'Search',
+    }),
+  },
   Add: {
     screen: View,
     navigationOptions: {
@@ -17,6 +40,14 @@ export default createBottomTabNavigator({
       },
     },
   },
-  Notifications,
-  Profile,
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: 'Notifications',
+    }),
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: 'Profile',
+    }),
+  },
 });
