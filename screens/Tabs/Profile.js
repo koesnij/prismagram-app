c6f2dfa8d2126631e8e15f526bf38ad7f7ae2b9e
koesnij <m_jinseok@naver.com>
2020-09-17 18:11:23 +0900

    #17.0 Profile Screen

--- screens/Tabs/Profile.js
+++ screens/Tabs/Profile.js
@@ -1,24 +1,68 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
+import { gql } from 'apollo-boost';
 import styled from 'styled-components';
+
 import AuthButton from '../../components/AuthButton';
 import { useLogOut } from '../../AuthContext';
-
-const View = styled.View`
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-`;
+import { useQuery } from '@apollo/react-hooks';
+import Loader from '../../components/Loader';
+import UserProfile from '../../components/UserProfile';
 
 const Text = styled.Text``;
+
+export const ME = gql`
+  {
+    me {
+      id
+      avatar
+      username
+      fullName
+      isFollowing
+      isSelf
+      bio
+      followingCount
+      followersCount
+      postsCount
+      posts {
+        id
+        location
+        caption
+        user {
+          id
+          avatar
+          username
+        }
+        files {
+          id
+          url
+        }
+        likeCount
+        isLiked
+        comments {
+          id
+          text
+          user {
+            id
+            username
+          }
+        }
+        createdAt
+      }
+    }
+  }
+`;
 
 export default () => {
   const logOut = useLogOut();
   const handler = () => {
     logOut();
   };
+  const { loading, data } = useQuery(ME);
   return (
-    <View>
+    <ScrollView>
+      {loading ? <Loader /> : data && data.me && <UserProfile {...data.me} />}
       <AuthButton onPress={handler} text="Log Out" />
-    </View>
+    </ScrollView>
   );
 };
